a118840e9862a3afb834f316049c727fdb254096
1natsu172 <1natsu172@users.noreply.github.com>
2018-03-04 16:34:50 +0900

    :sparkles: Initialize
    ready git

---
+++ index.js
@@ -0,0 +1,58 @@
+'use strict'
+
+const checkElement = (selector) => {
+  return document.querySelector(selector)
+}
+
+module.exports = (selector, options) => {
+  options = Object.assign({
+    target: document,
+    timer: 0
+  }, options)
+
+  return new Promise((resolve, reject) => {
+    let hasDetected = false
+    let timeoutId
+
+    const configs = {
+      childList: true,
+      subtree: true
+    }
+
+    // Checking already element existed.
+    const element = checkElement(selector)
+    if (element) {
+      hasDetected = true
+      return resolve(element)
+    }
+
+    const observer = new MutationObserver(mutations => {
+      mutations.some(mutation => {
+        const element = checkElement(selector)
+
+        if (element) {
+          hasDetected = true
+          if (options.timer > 0) {
+            clearTimeout(timeoutId)
+          }
+          observer.disconnect()
+          resolve(element)
+          return true; // the same as "break" in `Array.some()`
+        }
+      })
+    })
+
+    // Start observe.
+    observer.observe(options.target, configs)
+
+    // Set timer.
+    if (options.timer > 0 && !hasDetected) {
+      timeoutId = setTimeout(() => {
+        if (!hasDetected) {
+          observer.disconnect()
+          reject(new Error(`Element was not found: ${selector}`))
+        }
+      }, options.timer)
+    }
+  })
+}
---
+++ readme.md
@@ -0,0 +1,45 @@
+# wait-element [![Build Status](https://travis-ci.org/https://github.com/1natsu172/wait-element.svg?branch=master)](https://travis-ci.org/https://github.com/1natsu172/wait-element)
+
+>
+
+
+## Install
+
+```
+$ npm install wait-element
+```
+
+
+## Usage
+
+```js
+const <%= camelModuleName %> = require('wait-element');
+
+<%= camelModuleName %>('unicorns');
+//=> 'unicorns & rainbows'
+```
+
+
+## API
+
+### <%= camelModuleName %>(input, [options])
+
+#### input
+
+Type: `string`
+
+Lorem ipsum.
+
+#### options
+
+##### foo
+
+Type: `boolean`<br>
+Default: `false`
+
+Lorem ipsum.
+
+
+## License
+
+MIT © [1natsu172](https://github.com/https://github.com/1natsu172)
