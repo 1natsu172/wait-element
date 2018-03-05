548bcbd8220bd0f3d1512961074f674f222a704b
1natsu172 <1natsu172@users.noreply.github.com>
2018-03-05 17:49:50 +0900

    Run XO fix & prettier

--- index.js
+++ index.js
@@ -1,58 +1,60 @@
-'use strict'
+'use strict';
 
-const checkElement = (selector) => {
-  return document.querySelector(selector)
-}
+const checkElement = selector => {
+	return document.querySelector(selector);
+};
 
 module.exports = (selector, options) => {
-  options = Object.assign({
-    target: document,
-    timeout: 0
-  }, options)
+	options = Object.assign({
+		target: document,
+		timeout: 0
+	}, options);
 
-  return new Promise((resolve, reject) => {
-    let hasDetected = false
-    let timeoutId
+	return new Promise((resolve, reject) => {
+		let hasDetected = false;
+		let timeoutId;
 
-    const configs = {
-      childList: true,
-      subtree: true
-    }
+		const configs = {
+			childList: true,
+			subtree: true
+		};
 
     // Checking already element existed.
-    const element = checkElement(selector)
-    if (element) {
-      hasDetected = true
-      return resolve(element)
-    }
+		const element = checkElement(selector);
+		if (element) {
+			hasDetected = true;
+			return resolve(element);
+		}
 
-    const observer = new MutationObserver(mutations => {
-      mutations.some(mutation => {
-        const element = checkElement(selector)
+		const observer = new MutationObserver(mutations => {
+			mutations.some(() => {
+				const element = checkElement(selector);
 
-        if (element) {
-          hasDetected = true
-          if (options.timeout > 0) {
-            clearTimeout(timeoutId)
-          }
-          observer.disconnect()
-          resolve(element)
-          return true; // the same as "break" in `Array.some()`
-        }
-      })
-    })
+				if (element) {
+					hasDetected = true;
+					if (options.timeout > 0) {
+						clearTimeout(timeoutId);
+					}
+					observer.disconnect();
+					resolve(element);
+					return true; // The same as "break" in `Array.some()`
+				}
+
+				return false;
+			});
+		});
 
     // Start observe.
-    observer.observe(options.target, configs)
+		observer.observe(options.target, configs);
 
     // Set timeout.
-    if (options.timeout > 0 && !hasDetected) {
-      timeoutId = setTimeout(() => {
-        if (!hasDetected) {
-          observer.disconnect()
-          reject(new Error(`Element was not found: ${selector}`))
-        }
-      }, options.timeout)
-    }
-  })
-}
+		if (options.timeout > 0 && !hasDetected) {
+			timeoutId = setTimeout(() => {
+				if (!hasDetected) {
+					observer.disconnect();
+					reject(new Error(`Element was not found: ${selector}`));
+				}
+			}, options.timeout);
+		}
+	});
+};
--- rollup.config.js
+++ rollup.config.js
@@ -3,29 +3,29 @@ import nodeResolve from 'rollup-plugin-node-resolve';
 import babel from 'rollup-plugin-babel';
 
 export default {
-  input: 'index.js',
-  output: [
-    {
-      file: 'dist/cjs/index.js',
-      format: 'cjs'
-    },
-    {
-      file: 'dist/es/index.js',
-      format: 'es'
-    }
-  ],
-  plugins: [
-    nodeResolve({
-      jsnext: true,
-      main: true
-    }),
+	input: 'index.js',
+	output: [
+		{
+			file: 'dist/cjs/index.js',
+			format: 'cjs'
+		},
+		{
+			file: 'dist/es/index.js',
+			format: 'es'
+		}
+	],
+	plugins: [
+		nodeResolve({
+			jsnext: true,
+			main: true
+		}),
 
-    commonjs({
-      sourceMap: false,  // Default: true
-    }),
+		commonjs({
+			sourceMap: false  // Default: true
+		}),
 
-    babel({
-      plugins: ['external-helpers']
-    })
-  ]
+		babel({
+			plugins: ['external-helpers']
+		})
+	]
 };
