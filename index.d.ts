cb5af2ff821d5182978fc93539a8402c45cf2538
1natsu172 <1natsu172@users.noreply.github.com>
2018-11-08 13:25:12 +0900

    :children_crossing: feat: add TypeScript type definition
    Completely TypeScript support. #1

--- index.d.ts
+++ index.d.ts
@@ -1,1 +1,60 @@
-declare module '@1natsu/wait-element';
+declare module '@1natsu/wait-element' {
+	// Type definitions for "@1natsu/wait-element" v2.0.0
+	// Project: "@1natsu/wait-element"
+	// Definitions by: 1natsu <https://github.com/1natsu172>
+
+	/*~ Note that ES6 modules cannot directly export callable functions.
+ *~ This file should be imported using the CommonJS-style:
+ *~   import x = require('someLibrary');
+ *~
+ *~ Refer to the documentation to understand common
+ *~ workarounds for this limitation of ES6 modules.
+ */
+
+	/*~ This declaration specifies that the function
+ *~ is the exported object from the file
+ */
+	export = waitElement
+
+	/*~ multiple overloads for function */
+	/**
+	 *
+	 * @param selector
+	 * @param options
+	 * @param {string} [options.target] foo
+	 */
+	declare function waitElement<K extends keyof HTMLElementTagNameMap>(
+		selector: K,
+		options?: waitElement.options
+	): waitElement.CancelablePromise<HTMLElementTagNameMap[K] | null>
+
+	declare function waitElement<K extends keyof SVGElementTagNameMap>(
+		selector: K,
+		options?: waitElement.options
+	): waitElement.CancelablePromise<SVGElementTagNameMap[K] | null>
+
+	declare function waitElement<E extends Element = Element>(
+		selector: string,
+		options?: waitElement.options
+	): waitElement.CancelablePromise<E | null>
+
+	/**
+	 * namespace
+	 */
+	declare namespace waitElement {
+		export interface CancelablePromise<T> extends Promise<T> {
+			cancel(): void
+		}
+
+		/**
+		 *
+		 * @export
+		 * @interface options
+		 * @typedef {object}
+		 */
+		export interface options {
+			target?: Node
+			timeout?: number
+		}
+	}
+}
