fdbb945bd67da94b8a6373e802a79145b64b04fb
1natsu <1natsu172@users.noreply.github.com>
2021-02-15 19:54:35 +0900

    feat: rewrite by typescript

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,70 @@
-import deepmerge from 'deepmerge'
 import PCancelable from 'p-cancelable'
+import { Options, defaultOptions, mergeOptions, NodeLike } from './options'
 
-type Options = {
-	target: Document,
-	timeout: number,
-	observeConfigs: MutationObserverInit
-}
 
-const defaultOptions: Options = {
-	target: document,
-	timeout: 0,
-	observeConfigs: {
-		childList: true,
-		subtree: true,
-		attributes: true
-	}
-}
-
-export function waitElement(selector: string, _options?: Partial<Options>): PCancelable<Element> {
-
-	const options = deepmerge(defaultOptions, _options || {})
+export function waitElement<Target extends NodeLike = Document>(
+	selector: string,
+	_options?: Partial<Options>,
+): PCancelable<Element> {
+	const options = _options
+		? mergeOptions(defaultOptions(), _options)
+		: defaultOptions()
 
 	const checkElement = (selector: string) => {
-		return options.target.querySelector(selector);
-	};
+		return options.target.querySelector(selector)
+	}
 
 	return new PCancelable((resolve, reject, onCancel) => {
-		let observer: MutationObserver;
-		let _hasObserved = false;
-		let _timeoutId: ReturnType<typeof setTimeout>;
+		let observer: MutationObserver
+		let _hasObserved = false
+		let _timeoutId: ReturnType<typeof setTimeout>
 
 		onCancel(() => {
 			if (_timeoutId) {
-				clearTimeout(_timeoutId);
+				clearTimeout(_timeoutId)
 			}
 
-			observer.disconnect();
-			_hasObserved = true;
-		});
+			observer.disconnect()
+			_hasObserved = true
+		})
 
 		// Checking already element existed.
-		const element = checkElement(selector);
+		const element = checkElement(selector)
 		if (element) {
-			_hasObserved = true;
-			return resolve(element);
+			_hasObserved = true
+			return resolve(element)
 		}
 
-		observer = new MutationObserver(mutations => {
+		observer = new MutationObserver((mutations) => {
 			mutations.some(() => {
-				const element = checkElement(selector);
+				const element = checkElement(selector)
 
 				if (element) {
 					if (_timeoutId) {
-						clearTimeout(_timeoutId);
+						clearTimeout(_timeoutId)
 					}
 
-					observer.disconnect();
-					_hasObserved = true;
-					resolve(element);
-					return true; // The same as "break" in `Array.some()`
+					observer.disconnect()
+					_hasObserved = true
+					resolve(element)
+					return true // The same as "break" in `Array.some()`
 				}
 
-				return false;
-			});
-		});
+				return false
+			})
+		})
 
 		// Start observe.
-		observer.observe(options.target, options.observeConfigs);
+		observer.observe(options.target, options.observeConfigs)
 
 		// Set timeout.
 		if (options.timeout > 0 && !_hasObserved) {
 			_timeoutId = setTimeout(() => {
 				if (!_hasObserved) {
-					observer.disconnect();
-					reject(new Error(`Element was not found: ${selector}`));
+					observer.disconnect()
+					reject(new Error(`Element was not found: ${selector}`))
 				}
-			}, options.timeout);
+			}, options.timeout)
 		}
-	});
-};
+	})
+}
