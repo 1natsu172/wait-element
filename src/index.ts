e25c40a6368de26fcd4b75f01ad0c83d68f17d77
1natsu <1natsu172@users.noreply.github.com>
2021-02-15 23:55:49 +0900

    feat: implement waitDisappearElement

--- src/index.ts
+++ src/index.ts
@@ -1,69 +1,75 @@
 import PCancelable from 'p-cancelable'
-import {Options, defaultOptions, mergeOptions} from './options'
+import { Options, defaultOptions, mergeOptions } from './options'
+import {DetectConditionMatcher,isAppeared,isDisappeared} from './detectConditions'
 
-export function waitElement(
-	selector: string,
-	_options?: Partial<Options>,
-): PCancelable<Element> {
-	const options = _options
-		? mergeOptions(defaultOptions(), _options)
-		: defaultOptions()
+function createWaitElement<Result extends Element|null>(isMatchDetectCondition:DetectConditionMatcher<Result>) {
+	return function(
+		selector: string,
+		_options?: Partial<Options>,
+	): PCancelable<Result> {
+		const options = _options
+			? mergeOptions(defaultOptions(), _options)
+			: defaultOptions()
 
-	const checkElement = (selector: string) => {
-		return options.target.querySelector(selector)
-	}
+		const checkElement = (selector: string) => {
+			return options.target.querySelector(selector)
+		}
 
-	return new PCancelable((resolve, reject, onCancel) => {
-		let _hasObserved = false
-		let _timeoutId: ReturnType<typeof setTimeout>
+		return new PCancelable((resolve, reject, onCancel) => {
+			let _hasObserved = false
+			let _timeoutId: ReturnType<typeof setTimeout>
 
-		const observer: MutationObserver = new MutationObserver((mutations) => {
-			mutations.some(() => {
-				const element = checkElement(selector)
+			const observer: MutationObserver = new MutationObserver((mutations) => {
+				mutations.some(() => {
+					const element = checkElement(selector)
 
-				if (element) {
-					if (_timeoutId) {
-						clearTimeout(_timeoutId)
+					if (isMatchDetectCondition(element)) {
+						if (_timeoutId) {
+							clearTimeout(_timeoutId)
+						}
+
+						observer.disconnect()
+						_hasObserved = true
+						resolve(element)
+						return true // The same as "break" in `Array.some()`
 					}
 
-					observer.disconnect()
-					_hasObserved = true
-					resolve(element)
-					return true // The same as "break" in `Array.some()`
+					return false
+				})
+			})
+
+			onCancel(() => {
+				if (_timeoutId) {
+					clearTimeout(_timeoutId)
 				}
 
-				return false
+				observer.disconnect()
+				_hasObserved = true
 			})
-		})
 
-		onCancel(() => {
-			if (_timeoutId) {
-				clearTimeout(_timeoutId)
+			// Checking already element existed.
+			const element = checkElement(selector)
+			if (isMatchDetectCondition(element)) {
+				_hasObserved = true
+				resolve(element)
+				return
 			}
 
-			observer.disconnect()
-			_hasObserved = true
+			// Start observe.
+			observer.observe(options.target, options.observeConfigs)
+
+			// Set timeout.
+			if (options.timeout > 0 && !_hasObserved) {
+				_timeoutId = setTimeout(() => {
+					if (!_hasObserved) {
+						observer.disconnect()
+						reject(new Error(`Element was not found: ${selector}`))
+					}
+				}, options.timeout)
+			}
 		})
-
-		// Checking already element existed.
-		const element = checkElement(selector)
-		if (element) {
-			_hasObserved = true
-			resolve(element)
-			return
-		}
-
-		// Start observe.
-		observer.observe(options.target, options.observeConfigs)
-
-		// Set timeout.
-		if (options.timeout > 0 && !_hasObserved) {
-			_timeoutId = setTimeout(() => {
-				if (!_hasObserved) {
-					observer.disconnect()
-					reject(new Error(`Element was not found: ${selector}`))
-				}
-			}, options.timeout)
-		}
-	})
+	}
 }
+
+export const waitElement  = createWaitElement(isAppeared)
+export const waitDisappearElement  = createWaitElement(isDisappeared)
