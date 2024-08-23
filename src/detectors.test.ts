7591ef7d674b9e1ceba8f9e8e8cc97cb93f4738c
1natsu <1natsu172@users.noreply.github.com>
2024-08-23 20:00:33 +0900

    test: prepare detector tests

--- src/detectors.test.ts
+++ src/detectors.test.ts
@@ -1,24 +1,23 @@
 import { describe, test } from "vitest";
+import { waitElement } from ".";
 import { isExist, isNotExist } from "./detectors";
 
 describe("detectors", () => {
 	describe(isExist.name, () => {
-		test("should detect by", () => {
-			const result = isExist({
-				element: document.createElement("a"),
-				cache: new WeakMap(),
-			});
-			t.true(result);
+		test("should detect by", async ({ expect }) => {
+			document.body.append(document.createElement("a"));
+
+			const result = await waitElement("a", { detector: isExist });
+
+			expect(result).toBeInstanceOf(Element);
 		});
 	});
 
 	describe(isNotExist.name, () => {
-		test("should detect by", () => {
-			const result = isNotExist({
-				element: document.createElement("a"),
-				cache: new WeakMap(),
-			});
-			t.true(result);
+		test("should detect by", async ({ expect }) => {
+			const result = await waitElement(".hero", { detector: isNotExist });
+
+			expect(result).toBe(null);
 		});
 	});
 });
